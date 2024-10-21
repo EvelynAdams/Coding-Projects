798a4ce08352c5dc7563b19dee0b7fecfa802ad9
Evelyn Adams <111076192+EvelynAdams@users.noreply.github.com>
2024-10-21 19:40:08 -0400

    Python

---
+++ Novice/TicTacToe.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+class TicTacToe{
+  void displayBoard(){
+
+  }
+
+  void playerMove(char player){
+
+  }
+
+  boolean  checkWin(){
+
+  }
+
+  void playGame(){
+
+  }
+
+  public static void main(String args[]){
+    // prompt user to play
+    System.out.println("Do you wish to play Tic Tac Toe? (Y/N) ");
+    Scanner myObj = new Scanner(System.in);
+    char selection = myObj.next().charAt(0);
+
+    if(selection.comareToIgnoreCase("Y"))
+
+    //handle selection
+
+
+
+  }
+
+  }
+
+  public class Board{
+    private char [] board;
+
+    // constructor to create initial tic tac toe board
+    board(){
+      this.board = new char []{"-", "-", "-", "-", "-", "-", "-", "-", "-"};
+    }
+
+    // get space status
+    public spaceStaus(char space){
+      
+    }
+
+    // add move
+
+    // display board
+  }
+
+}
